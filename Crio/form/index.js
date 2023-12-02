e5c1aabb7ce0536971c88905bd243622b2c052c4
GajananBodhankar <bodhankargajanan99@gmail.com>
2023-12-02 16:55:35 +0530

    done with form validation

--- Crio/form/index.js
+++ Crio/form/index.js
@@ -1,18 +1,22 @@
+let data = { name: null, email: null, doc: null, where: null, when: null };
 let x = document.getElementById("form");
 x.addEventListener("submit", (e) => {
   e.preventDefault();
   send();
+  global();
 });
 function send() {
   let name = document.getElementById("form");
-  console.dir(name);
-  console.log(name.elements["Email"].value);
+  // console.dir(name);
+  // console.log(name.elements["Email"].value);
 }
 let fname = document.querySelector("#fname");
 fname.addEventListener("input", function (e) {
   nameCheck(e.target.value);
 });
 fname.addEventListener("focus", function (e) {
+  eraseWarn("mainwarn");
+
   nameCheck(e.target.value);
 });
 fname.addEventListener("blur", function (e) {
@@ -23,6 +27,7 @@ email.addEventListener("input", function (e) {
   checkEmail(e.target.value);
 });
 email.addEventListener("focus", function (e) {
+  eraseWarn("mainwarn");
   checkEmail(e.target.value);
 });
 email.addEventListener("blur", function (e) {
@@ -39,6 +44,7 @@ function nameCheck(value) {
     document.querySelector("#fnamewarn").textContent =
       "Format should be first name and last name with only alphabets.";
   } else {
+    data.name = value;
     document.querySelector("#fnamewarn").style.visibility = "hidden";
     document.querySelector("#fnamewarn").textContent = "hii";
   }
@@ -53,10 +59,11 @@ function checkEmail(value) {
   } else {
     document.querySelector("#emailwarn").style.visibility = "hidden";
     document.querySelector("#emailwarn").textContent = "hii";
+    data.email = value;
   }
 }
-async function api() {
-  let data = { name: "gajanan" };
+async function api(name, email, doc, where, when) {
+  let data = { name, email, doc, where, when };
   let s = await fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     body: JSON.stringify(data),
@@ -64,6 +71,47 @@ async function api() {
       "Content-Type": "application/json",
     },
   });
-  s.json().then((e) => console.log(e));
+  s.json().then((e) => {
+    console.log(e);
+    alert("Submitted the data");
+  });
 }
-api();
+
+let doc = document.querySelector("#doctor");
+doc.addEventListener("click", function (e) {
+  if (e.target.value) {
+    data.doc = e.target.value;
+  }
+});
+
+let google = document.getElementById("Google Meet");
+google.addEventListener("click", function (e) {
+  data.where = e.target.value;
+});
+
+let phone = document.getElementById("Phone");
+phone.addEventListener("click", function (e) {
+  data.where = e.target.value;
+});
+
+let d = document.getElementById("date");
+d.addEventListener("input", function (e) {
+  data.when = e.target.value;
+});
+
+function global() {
+  let flag = false;
+  for (let i in data) {
+    if (data[i] == null) {
+      flag = true;
+    }
+  }
+  if (flag) {
+    document.getElementById("mainwarn").textContent =
+      "Please fill all the details";
+    document.getElementById("mainwarn").style.visibility = "visible";
+  } else {
+    api(data.name, data.email, data.doc, data.where, data.when);
+    eraseWarn("mainwarn");
+  }
+}
